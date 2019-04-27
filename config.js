2ada6eaff963092e3c4a04aea666b6e5ac1cf4c2
Taegus <taegus.cromis@gmail.com>
2019-04-27 11:18:57 +0200

    updated config.js

--- config.js
+++ config.js
@@ -6,20 +6,25 @@ var symbol = 'CCX';
 var refreshDelay = 5000;
 // pools stats by MainCoins
 var networkStat = {
- "ccx": [
-	["walpool.com/ccx", "https://api.walpool.com/rpc/ccx/"],
-	["pool.conceal.network", "https://pool.conceal.network/api"],
-	["ccx.scecf.org", "https://ccx.scecf.org:21001"],
-	["ccx.bluerockpools.net", "https://ccx.bluerockpools.net:8119"],
-	["conceal.herominers.com", "https://conceal.herominers.com/api"],	
-	["conceal.modpool.org", "https://conceal.modpool.org:9119"],
-	["ccx.thorshammer.cc", "https://ccx.thorshammer.cc/api"],
-
-]
+	"ccx": [
+        ["ccx.aiwakuang.cn", "https://ccx.aiwakuang.cn:8111"],
+        ["pool.conceal.network", "https://pool.conceal.network/api"],
+        ["ccx.scecf.org", "https://ccx.scecf.org:21001"],
+        ["ccx-us.bluerockpools.net", "https://ccx-us.bluerockpools.net:8119"],
+        ["conceal.herominers.com", "https://conceal.herominers.com/api"],
+        ["conceal.modpool.org", "https://conceal.modpool.org:9119"],
+        ["ccx.thorshammer.cc", "https://ccx.thorshammer.cc/api"],
+        ["conceal.my-mining-pool.de", "https://apiconceal.my-mining-pool.de"],
+        ["conceal.miningpros.online", "http://mine.miningpros.online:8117"],
+        ["conceal.blockfoundry.org", "https://conceal.blockfoundry.org/api"]
+	]
 };
 var networkStat2 = {
     "ccx": [
-		["ccx.go-mine.it", "https://ccx.go-mine.it/api"],		
+        ["ccx.go-mine.it", "https://ccx.go-mine.it/api"],
+        ["ccx.romining.farm", "https://ccx.romining.farm/api"],
+        ["webccx.semipool.com", "https://webccx.semipool.com/api"],
+        ["conceal.luckypool.io", "https://conceal.luckypool.io/api"]
  ]
 };
 var fundsProof = {
