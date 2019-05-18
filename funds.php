1c91d01c87c4e3b1096da9f7810f0b43934207c8
Taegus <taegus.cromis@gmail.com>
2019-05-18 19:32:15 +0200

    much better error handling notify of success

--- funds.php
+++ funds.php
@@ -1,11 +1,20 @@
 <?php
   if  ($_POST['uploadKey'] == "c394f380-c7df-479f-b312-0b4a6fc3a357")
   {
-	unlink('funds.dat.old');
-    rename('funds.dat', 'funds.dat.old') ;
-    file_put_contents('funds.dat', $_POST['fundsData']);	  
-    echo "true";
+	  if (!unlink('funds.dat.old')) {
+      echo json_encode(array('success' => false, 'reason' => 'Failed to delete the old funds.dat!'));
+    } else {
+      if (!rename('funds.dat', 'funds.dat.old')) {
+        echo json_encode(array('success' => false, 'reason' => 'Failed to rename the funds.dat!'));
+      } else {
+        if (file_put_contents('funds.dat', $_POST['fundsData']) === false) {
+          echo json_encode(array('success' => false, 'reason' => 'Failed to write the new funds.dat!'));
+        } else {
+          echo json_encode(array('success' => true));
+        }
+      }
+    }
   } else {
-    echo "false";
+    echo json_encode(array('success' => false, 'reason' => 'The upload key is wrong or not specified!'));
   } 
 ?>
