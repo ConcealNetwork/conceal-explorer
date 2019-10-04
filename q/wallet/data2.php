2fd0a5fa66ad238ad5a9f1f857bb630e4c3bc9aa
darkharvest <kahthan@gmail.com>
2019-10-04 14:14:53 +0530

    Create data2.php

---
+++ q/wallet/data2.php
@@ -0,0 +1,20 @@
+<?php
+
+require '../util.php';
+$config = (require '../../config.php');
+
+$bitstamp = fetch_bitstamp($config['bitstamp']);
+$bitstampeur = fetch_bitstamp($config["https://www.bitstamp.net/api/v2/ticker/btceur/"]);
+$btc = number_format($bitstamp['last'],2,".",",");
+$btceur = number_format($bitstampeur['last'],2,".",",");
+
+$coingecko = fetch_coingecko("https://api.coingecko.com/api/v3/coins/conceal");
+$ccxusd = number_format($coingecko['market_data']['current_price']['usd'],2,".",",");
+$marketcap = number_format($coingecko['market_data']['market_cap']['usd'],2,".",",");
+$totalvolume = number_format($coingecko['market_data']['total_volume']['usd'],2,".",",");
+$ccxbtc = number_format(($coingecko['market_data']['current_price']['btc'])*100000000,0,".",",");
+$ccxeur = number_format($coingecko['market_data']['current_price']['eur'],2,".",",");
+$marketcapeur = number_format($coingecko['market_data']['market_cap']['eur'],2,".",",");
+$totalvolumeeur = number_format($coingecko['market_data']['total_volume']['eur'],2,".",",");
+print_r($ccxbtc." ".$ccxusd." ".$btc." ".$marketcap." ".$totalvolume." ".$ccxeur." "$btceur." ".$marketcapeur." ".$totalvolumeeur;
+
