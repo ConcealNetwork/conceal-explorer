09c2dea5811201e56545dea28bfe1d44a50c494e
Taegus <taegus.cromis@gmail.com>
2020-07-08 19:38:39 +0200

    node count is actually smart node count api added correct node count api

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
 return array(
         'api' => 'https://explorer.conceal.network/daemon',
         'pool' => 'https://explorer.conceal.network/pool',
+        'services' => 'https://explorer.conceal.network/services',
         'blockTargetInterval' => 120,
         'coinUnits' => 1000000,
         'maple' => 'https://maplechange.com/api/v2/tickers/ccxbtc.json',
--- q/smartnodecount/index.php
+++ q/smartnodecount/index.php
@@ -2,7 +2,7 @@
 require '../util.php';
 $config = (require '../../config.php');
 
-$_url = $config['services'] . '/nodes/geodata';
+$_url = $config['pool'] . '/count';
 $response = json_decode(file_get_contents($_url), true);
 
-print_r(count($response));
+print_r($response['count']);
