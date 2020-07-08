187a414419a044321a0e10749940926d12f971f7
Taegus <taegus.cromis@gmail.com>
2020-07-08 19:39:46 +0200

    remove old settings

--- config.php
+++ config.php
@@ -4,10 +4,6 @@ return array(
         'pool' => 'https://explorer.conceal.network/pool',
         'services' => 'https://explorer.conceal.network/services',
         'blockTargetInterval' => 120,
-        'coinUnits' => 1000000,
-        'maple' => 'https://maplechange.com/api/v2/tickers/ccxbtc.json',
-        'bitstamp' => 'https://www.bitstamp.net/api/ticker/'
-
-        
+        'coinUnits' => 1000000
 );
 ?>
